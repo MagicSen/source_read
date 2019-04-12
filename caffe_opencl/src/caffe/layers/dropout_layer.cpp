55ba67e5905e6aa46df66a6420570cabe29e3b9e
MagicSen <magicys@qq.com>
2019-04-12 20:27:55 +0800

    添加dropout注释

--- caffe_opencl/src/caffe/layers/dropout_layer.cpp
+++ caffe_opencl/src/caffe/layers/dropout_layer.cpp
@@ -26,9 +26,11 @@ void DropoutLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 void DropoutLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
+  // 上下层结构输入一致
   NeuronLayer<Dtype>::Reshape(bottom, top);
   // Set up the cache for random number generation
   // ReshapeLike does not work because rand_vec_ is of Dtype uint
+  // 生成随机数，放置到缓存里
   rand_vec_.Reshape(bottom[0]->shape());
 }
 
@@ -41,11 +43,14 @@ void DropoutLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   const int_tp count = bottom[0]->count();
   if (this->phase_ == TRAIN) {
     // Create random numbers
+    // 0~1 伯努利分布，生成随机数， 0101011101
     caffe_rng_bernoulli(count, 1. - threshold_, mask);
     for (int_tp i = 0; i < count; ++i) {
+      // scale保证 数据总量 一致
       top_data[i] = bottom_data[i] * mask[i] * scale_;
     }
   } else {
+    // 测试过程下，直接赋值
     caffe_cpu_copy(bottom[0]->count(), bottom_data, top_data);
   }
 }
@@ -58,12 +63,14 @@ void DropoutLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const Dtype* top_diff = top[0]->cpu_diff();
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
     if (this->phase_ == TRAIN) {
+      // rand_vec_ 记录 dropout过程中的舍弃的位置
       const uint_tp* mask = rand_vec_.cpu_data();
       const int_tp count = bottom[0]->count();
       for (int_tp i = 0; i < count; ++i) {
         bottom_diff[i] = top_diff[i] * mask[i] * scale_;
       }
     } else {
+      // 测试阶段
       caffe_cpu_copy(top[0]->count(), top_diff, bottom_diff);
     }
   }
