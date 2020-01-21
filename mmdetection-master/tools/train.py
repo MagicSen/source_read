53500f3702f23d183bb80a2e93535f8b35334b8b
MagicSen <magicys@qq.com>
2020-01-21 16:33:19 +0800

    添加mmcv库

--- mmdetection-master/tools/train.py
+++ mmdetection-master/tools/train.py
@@ -15,32 +15,42 @@ from mmdet.datasets import build_dataset
 from mmdet.models import build_detector
 
 
+# 定义训练接收的变量及参数
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
+    # config 文件位置
     parser.add_argument('config', help='train config file path')
+    # 工作目录
     parser.add_argument('--work_dir', help='the dir to save logs and models')
+    # 重新训练的目录
     parser.add_argument(
         '--resume_from', help='the checkpoint file to resume from')
+    # 训练期间，是否评估验证集
     parser.add_argument(
         '--validate',
         action='store_true',
         help='whether to evaluate the checkpoint during training')
+    # 设置第几个GPU
     parser.add_argument(
         '--gpus',
         type=int,
         default=1,
         help='number of gpus to use '
         '(only applicable to non-distributed training)')
+    # 设置随机种子
     parser.add_argument('--seed', type=int, default=None, help='random seed')
+    # 是否确定使用CUDNN
     parser.add_argument(
         '--deterministic',
         action='store_true',
         help='whether to set deterministic options for CUDNN backend.')
+    # 分布式训练采用的工作调度器类型
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
         help='job launcher')
+    # 未确认的信息
     parser.add_argument('--local_rank', type=int, default=0)
     parser.add_argument(
         '--autoscale-lr',
@@ -54,12 +64,14 @@ def parse_args():
 
 
 def main():
+    # 从输入构建配置选项
     args = parse_args()
-
+    # 解析配置文件
     cfg = Config.fromfile(args.config)
     # set cudnn_benchmark
     if cfg.get('cudnn_benchmark', False):
         torch.backends.cudnn.benchmark = True
+    # 如果config没有设置某些参数，进行默认参数设置
     # update configs according to CLI args
     if args.work_dir is not None:
         cfg.work_dir = args.work_dir
@@ -67,6 +79,7 @@ def main():
         cfg.resume_from = args.resume_from
     cfg.gpus = args.gpus
 
+    # 设置优化器的学习率
     if args.autoscale_lr:
         # apply the linear scaling rule (https://arxiv.org/abs/1706.02677)
         cfg.optimizer['lr'] = cfg.optimizer['lr'] * cfg.gpus / 8
@@ -78,6 +91,7 @@ def main():
         distributed = True
         init_dist(args.launcher, **cfg.dist_params)
 
+    # 获得绝对路径，设置日志，打印配置信息
     # create work_dir
     mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
     # init the logger before other steps
@@ -90,18 +104,23 @@ def main():
     logger.info('MMDetection Version: {}'.format(__version__))
     logger.info('Config:\n{}'.format(cfg.text))
 
+    # 设置随机种子
     # set random seeds
     if args.seed is not None:
         logger.info('Set random seed to {}, deterministic: {}'.format(
             args.seed, args.deterministic))
         set_random_seed(args.seed, deterministic=args.deterministic)
 
+    # 基于config文件，创建模型
     model = build_detector(
         cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
 
+    # 创建数据集
     datasets = [build_dataset(cfg.data.train)]
+    # 如果有两个workflow，设置验证集
     if len(cfg.workflow) == 2:
         datasets.append(build_dataset(cfg.data.val))
+    # 设置checkpoint配置文件
     if cfg.checkpoint_config is not None:
         # save mmdet version, config file content and class names in
         # checkpoints as meta data
@@ -109,8 +128,10 @@ def main():
             mmdet_version=__version__,
             config=cfg.text,
             CLASSES=datasets[0].CLASSES)
+    # 获取类的list
     # add an attribute for visualization convenience
     model.CLASSES = datasets[0].CLASSES
+    # 开启训练
     train_detector(
         model,
         datasets,
